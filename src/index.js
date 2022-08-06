cb8c13d08c17561b36edd94f764065f7048d58a5
Konstantin <kos8533@yandex.ru>
2022-08-06 18:32:23 +0300

    added connect to mongoDB

--- src/index.js
+++ src/index.js
@@ -1,11 +1,33 @@
 const express = require('express');
+const mongoose = require('mongoose');
 
 const app = express();
 
 const PORT = process.env.PORT || 3002;
+const UrlDB = process.env.UrlDB || 'mongodb+srv://Konstantin:Q0s1lJxNxjIG7UOW@cluster0.3gx3nos.mongodb.net/?retryWrites=true&w=majority'
+
+async function start(PORT, UrlDB) {
+    try {
+        await mongoose
+            .connect(UrlDB)
+            .then(() => {
+                console.info('Connected to MONGO.');
+            })
+            .catch((error) => {
+                console.error('Failed to connect to: MONGO.', error);
+
+                return process.exit(1);
+            });
+
+        app.listen(PORT, () => { `Server is running on port ${PORT}` })
+    }
+    catch (e) {
+        console.log(e)
+    }
+}
 
 app.use('/', (req, res) => {
     res.json('Delivery Service')
 })
 
-app.listen(PORT, () => {`Server is running on port ${PORT}`});
+start(PORT, UrlDB)
